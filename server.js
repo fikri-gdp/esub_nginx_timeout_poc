70a7fe715e9689ee9d1a0531604d8e303019080d
arroganthooman <fikriakmal453@gmail.com>
2025-01-16 22:01:41 +0700

    add more log

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 app.get('/esubmission', async (req, res) => {
     console.log('Received request, delaying response...');
     await new Promise(resolve => setTimeout(resolve, 90000)); // 90 seconds delay
+    console.log("Success")
     res.status(200).send('Success after 90 seconds');
 });
 
